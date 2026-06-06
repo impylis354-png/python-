ad7bf759870332f8c65250ad42fd2f667f8c066f
Dmitry <impylis354@gmail.com>
2026-06-06 15:04:34 +0300

    add file

--- lab3.py
+++ lab3.py
@@ -2,57 +2,60 @@ import math
 
 
 def get_input_data():
-    """Ввод данных пользователем"""
     d1 = float(input("Введите кратчайшее расстояние между спасателем и кромкой воды, d1 (ярды) => "))
     d2 = float(input("Введите кратчайшее расстояние от утопающего до берега, d2 (футы) => "))
     h = float(input("Введите боковое смещение между спасателем и утопающим, h (ярды) => "))
     v_sand = float(input("Введите скорость движения спасателя по песку, v_sand (мили в час) => "))
     n = float(input("Введите коэффициент замедления спасателя при движении в воде, n => "))
-    theta1_deg = float(input("Введите направление движения спасателя по песку, theta1 (градусы) => "))
 
-    return d1, d2, h, v_sand, n, theta1_deg
+    return d1, d2, h, v_sand, n
 
 
 def calculate_time(d1, d2, h, v_sand, n, theta1_deg):
-    """Вычисление времени достижения утопающего"""
-
     theta1 = math.radians(theta1_deg)
 
     d1_ft = d1 * 3
     h_ft = h * 3
 
     x = d1_ft * math.tan(theta1)
+
     s1 = math.sqrt(d1_ft ** 2 + x ** 2)
     s2 = math.sqrt((h_ft - x) ** 2 + d2 ** 2)
 
     v_sand_fts = v_sand * 5280 / 3600
     v_water_fts = v_sand_fts / n
 
-    time = s1 / v_sand_fts + s2 / v_water_fts
-
-    return time
+    return s1 / v_sand_fts + s2 / v_water_fts
 
 
-def print_result(theta1_deg, time):
-    """Вывод результата"""
-    print(
-        f"\nЕсли спасатель начнёт движение под углом theta1, "
-        f"равным {theta1_deg:.0f} градусам, "
-        f"он достигнет утопающего через {time:.1f} секунды."
-    )
+def find_best_angle(d1, d2, h, v_sand, n):
+    best_angle = 0
+    best_time = float("inf")
+
+    angle = 0
+
+    while angle < 90:
+        current_time = calculate_time(
+            d1, d2, h, v_sand, n, angle
+        )
+
+        if current_time < best_time:
+            best_time = current_time
+            best_angle = angle
+
+        angle += 0.1
+
+    return best_angle, best_time
 
 
 # -------------------
 # МОДУЛЬНЫЕ ТЕСТЫ
 # -------------------
 
-assert round(calculate_time(8, 10, 50, 5, 2, 39.413), 1) == 39.9
-assert calculate_time(8, 10, 50, 5, 2, 39.413) > 0
+angle, time = find_best_angle(8, 10, 50, 5, 2)
 
-t1 = calculate_time(8, 10, 50, 5, 2, 39.413)
-t2 = calculate_time(8, 10, 50, 5, 4, 39.413)
-
-assert t2 > t1
+assert 0 <= angle <= 90
+assert time > 0
 
 print("Все тесты пройдены успешно!")
 
@@ -60,8 +63,12 @@ print("Все тесты пройдены успешно!")
 # ОСНОВНАЯ ПРОГРАММА
 # -------------------
 
-d1, d2, h, v_sand, n, theta1_deg = get_input_data()
+d1, d2, h, v_sand, n = get_input_data()
 
-time = calculate_time(d1, d2, h, v_sand, n, theta1_deg)
+best_angle, best_time = find_best_angle(
+    d1, d2, h, v_sand, n
+)
 
-print_result(theta1_deg, time)
+print(f"\nОптимальный угол: {best_angle:.1f} градусов")
+print(f"Минимальное время: {best_time:.1f} секунд")
+
